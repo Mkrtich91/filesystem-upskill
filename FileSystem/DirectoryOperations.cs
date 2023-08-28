bf09301e6be39ff85b3e9ba651f9d927606e8e83
Mkrtich Kazanchian <mkogeo@gmail.com>
2023-08-28 16:16:26 +0400

    Solved at 28.08mm

--- FileSystem/DirectoryOperations.cs
+++ FileSystem/DirectoryOperations.cs
@@ -1,43 +1,183 @@
-using System.IO;
-
 namespace FileSystem
 {
     public static class DirectoryOperations
     {
         public static void DirectoryIsCreatedOrNotValidateDirectory(string dirPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            if (!Directory.Exists(dirPath))
+            {
+                try
+                {
+                    _ = Directory.CreateDirectory(dirPath);
+                    Console.WriteLine($"Directory '{dirPath}' created successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred while creating the directory: {ex.Message}");
+                    throw;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Directory '{dirPath}' already exists.");
+            }
         }
 
         public static void DirectoryIsDeletedOrNotValidateDirectory(string dirPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            if (Directory.Exists(dirPath))
+            {
+                try
+                {
+                    Directory.Delete(dirPath);
+                    Console.WriteLine($"Directory '{dirPath}' deleted successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred while deleting the directory: {ex.Message}");
+                    throw;
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Directory '{dirPath}' does not exist.");
+            }
         }
 
         public static void DirectoryIsMovedToOtherDirectoryValidateDirectory(string sourceDirPath, string destinationDirPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                if (!Directory.Exists(sourceDirPath))
+                {
+                    Console.WriteLine("Source directory does not exist.");
+                    return;
+                }
+
+                if (!Directory.Exists(destinationDirPath))
+                {
+                    _ = Directory.CreateDirectory(destinationDirPath);
+                }
+
+                Directory.Move(sourceDirPath, Path.Combine(destinationDirPath, Path.GetFileName(sourceDirPath)));
+
+                if (Directory.Exists(Path.Combine(destinationDirPath, Path.GetFileName(sourceDirPath))))
+                {
+                    Console.WriteLine("Directory was moved and validated successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Directory move was not successful.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                throw;
+            }
         }
 
         public static void SubDirectoryIsCreatedOrNotValidateSubDirectory(string dirPath, string subDirPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                string subDirFullPath = Path.Combine(dirPath, subDirPath);
+
+                if (!Directory.Exists(dirPath))
+                {
+                    Console.WriteLine("Main directory does not exist.");
+                    return;
+                }
+
+                if (!Directory.Exists(subDirFullPath))
+                {
+                    _ = Directory.CreateDirectory(subDirFullPath);
+                    Console.WriteLine("Subdirectory created successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Subdirectory already exists.");
+                }
+
+                if (Directory.Exists(subDirFullPath))
+                {
+                    Console.WriteLine("Subdirectory validation successful.");
+                }
+                else
+                {
+                    Console.WriteLine("Subdirectory validation failed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                throw;
+            }
         }
 
         public static void DeleteSubDirectoryValidateSubDirectoryDeletedOrNot(string subDirPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                if (!Directory.Exists(subDirPath))
+                {
+                    Console.WriteLine("Subdirectory does not exist.");
+                    return;
+                }
+
+                Directory.Delete(subDirPath, true);
+
+                if (!Directory.Exists(subDirPath))
+                {
+                    Console.WriteLine("Subdirectory deleted successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Subdirectory deletion failed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                throw;
+            }
         }
 
         public static void MoveSubDirectoryValidateSubDirMovedOrNot(string sourcePath, string destinationPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                if (!Directory.Exists(sourcePath))
+                {
+                    Console.WriteLine("Source subdirectory does not exist.");
+                    return;
+                }
+
+                if (!Directory.Exists(destinationPath))
+                {
+                    _ = Directory.CreateDirectory(destinationPath);
+                }
+
+                string subDirName = new DirectoryInfo(sourcePath).Name;
+
+                string newSubDirPath = Path.Combine(destinationPath, subDirName);
+
+                Directory.Move(sourcePath, newSubDirPath);
+
+                if (Directory.Exists(newSubDirPath))
+                {
+                    Console.WriteLine("Subdirectory moved successfully.");
+                }
+                else
+                {
+                    Console.WriteLine("Subdirectory move failed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                throw;
+            }
         }
     }
 }
--- FileSystem/FileOperations.cs
+++ FileSystem/FileOperations.cs
@@ -1,5 +1,3 @@
-using System.IO;
-
 [assembly: CLSCompliant(false)]
 
 namespace FileSystem
@@ -8,38 +6,135 @@ namespace FileSystem
     {
         public static void CreatingFileAndReturnFilePath(string filePath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                using StreamWriter sw = File.CreateText(filePath);
+                sw.WriteLine("Hello, this is some content.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                throw;
+            }
         }
 
         public static void WriteTextToFileReadAppendedText(string filePath, string msgToWrite)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                using (StreamWriter writer = File.AppendText(filePath))
+                {
+                    writer.WriteLine(msgToWrite);
+                }
+
+                using StreamReader reader = File.OpenText(filePath);
+                string line;
+#pragma warning disable CS8600
+                while ((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+#pragma warning restore CS8600
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                throw;
+            }
         }
 
         public static string ReadingFileContentAndValidateText(string filePath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                return string.Join(Environment.NewLine, lines);
+            }
+            catch (Exception ex)
+            {
+                return "An error occurred: " + ex.Message;
+                throw;
+            }
         }
 
         public static void MoveFileFromOneFolderToNewFolderAndValidateFile(string filePath, string destinationPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+#pragma warning disable CS8604
+                _ = Directory.CreateDirectory(Path.GetDirectoryName(destinationPath));
+#pragma warning restore CS8604
+
+                File.Move(filePath, destinationPath);
+
+                if (File.Exists(destinationPath))
+                {
+                    Console.WriteLine("File moved successfully and exists at the destination.");
+                }
+                else
+                {
+                    Console.WriteLine("File was not moved successfully or does not exist at the destination.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                throw;
+            }
         }
 
         public static void CopyFileFromOneFolderToNewFolder(string filePath, string destinationPath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+#pragma warning disable CS8604
+                _ = Directory.CreateDirectory(Path.GetDirectoryName(destinationPath));
+#pragma warning restore CS8604
+
+                File.Copy(filePath, destinationPath, true);
+
+                if (File.Exists(destinationPath))
+                {
+                    Console.WriteLine("File copied successfully and exists at the destination.");
+                }
+                else
+                {
+                    Console.WriteLine("File was not copied successfully or does not exist at the destination.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+                throw;
+            }
         }
 
         public static void DeleteFileAndValidateFileExistOrNot(string filePath)
         {
-            // TODO Implement the method.
-            throw new NotImplementedException();
+            try
+            {
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine("File does not exist.");
+                    return;
+                }
+
+                File.Delete(filePath);
+
+                if (!File.Exists(filePath))
+                {
+                    Console.WriteLine("File deleted and validation succeeded.");
+                }
+                else
+                {
+                    Console.WriteLine("File deleted, but validation failed.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                throw;
+            }
         }
     }
 }
